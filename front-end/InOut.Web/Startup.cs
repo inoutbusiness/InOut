ff8aee07278174918347ffe4b335b841fa16893b
Meiteusz <contato.matheuswt@gmail.com>
2022-05-28 18:46:12 -0300

    Teste 1 - Commitando as primeiras alterações.

---
+++ front-end/InOut.Web/Startup.cs
@@ -0,0 +1,49 @@
+﻿
+namespace InOut.Web
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get;}
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Auto Mapper Configurations
+
+
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            //app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "Home",
+                    template: "{controller=Home}/{action=Index}");
+            });
+        }
+    }
+}
---
+++ tests/Service.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+
+namespace Service.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Moq Installed
+        }
+    }
+}
