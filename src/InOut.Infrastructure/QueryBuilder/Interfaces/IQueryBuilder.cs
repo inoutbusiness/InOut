9e0d25e16830b2d17d06cea0ff43ba42232767e7
Meiteusz <contato.matheuswt@gmail.com>
2022-06-22 14:05:35 -0300

    PR8 - QueryBuilder - Builder facilitador de queries SQL.

---
+++ src/InOut.Infrastructure/QueryBuilder/Interfaces/IQueryBuilder.cs
@@ -0,0 +1,10 @@
+﻿using InOut.Infrastructure.Context;
+
+namespace InOut.Infrastructure.QueryBuilder.Interfaces
+{
+    public interface IQueryBuilder
+    {
+        Task<IEnumerable<T>> ExecQueryBuilderAsync<T>(InOutContext context, string querySQL) where T : class, new();
+        IEnumerable<T> ExecQueryBuilder<T>(InOutContext context, string querySQL) where T : class, new();
+    }
+}
