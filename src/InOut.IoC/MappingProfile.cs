7d70ff70019f99524b72eab3f958bf2cbce03ac5
Meiteusz <contato.matheuswt@gmail.com>
2022-06-28 21:18:47 -0300

    PR9 - Configurar Auto Mapper - Parte 2.

--- src/InOut.IoC/MappingProfile.cs
+++ src/InOut.IoC/MappingProfile.cs
@@ -1,7 +1,20 @@
-﻿namespace InOut.IoC
-{
-    public abstract class MappingProfile
-    {
+﻿using AutoMapper;
+using InOut.Domain.DTOs;
+using InOut.Domain.Entities;
 
+namespace InOut.IoC
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>();
+        }
+
+        public static MapperConfiguration CreateMappingProfile()
+            => new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
     }
 }
