ba7aaa836936cb14ef19a9ee46daf793dfdc812b
Meiteusz <contato.matheuswt@gmail.com>
2022-06-28 21:13:26 -0300

    teste

--- src/InOut.IoC/MappingProfile.cs
+++ src/InOut.IoC/MappingProfile.cs
@@ -1,7 +1,16 @@
 ﻿namespace InOut.IoC
 {
-    public abstract class MappingProfile
+    public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>();
+        }
 
+        public static MapperConfiguration CreateMappingProfile()
+            => new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
     }
 }
